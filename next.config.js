c9cd6d32913e28350d401ba875543700fc6c6c8c
ahmedRAOUANE <122179718+ahmedRAOUANE@users.noreply.github.com>
2024-07-20 09:35:20 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
-module.exports = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-                port: '',
-                pathname: '/account123/**',
-            },
-        ],
-    },
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/account123/**',
+      },
+    ],
+  },
+  output: 'export',
+  basePath: '/DocuMate',
+  assetPrefix: '/DocuMate/',
+};
+
+module.exports = nextConfig;
